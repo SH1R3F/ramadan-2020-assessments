8a8b38fc7c9544cba04840baa688d612b17636e0
Mahmoud Shiref <sh1r3f@gmail.com>
2022-04-10 21:30:28 +0200

    All Tasks Done

--- server/data/video-requests.data.js
+++ server/data/video-requests.data.js
@@ -1,52 +1,80 @@
-var VideoRequest = require('./../models/video-requests.model');
+const VideoRequest = require('./../models/video-requests.model');
+const User = require ('./../models/user.model');
 
 module.exports = {
-  createRequest: (vidRequestData) => {
+createRequest: async (vidRequestData) => {
+    const authorId = vidRequestData.author_id;
+    if (authorId) {
+        const userObj = await User.findOne({ _id: authorId });
+        vidRequestData.author_name = userObj.author_name;
+        vidRequestData.author_email = userObj.author_email;
+    }
     let newRequest = new VideoRequest(vidRequestData);
     return newRequest.save();
-  },
+},
 
-  getAllVideoRequests: (top) => {
+getAllVideoRequests: (top) => {
     return VideoRequest.find({}).sort({ submit_date: '-1' }).limit(top);
-  },
+},
 
-  searchRequests: (topic) => {
-    return VideoRequest.find({ topic_title: topic })
-      .sort({ addedAt: '-1' })
-      .limit(top);
-  },
+searchRequests: (topic) => {
+    return VideoRequest.find({ topic_title: {
+    $regex: topic,
+    $options: /i/
+    } })
+    .sort({ addedAt: '-1' })
+},
 
-  getRequestById: (id) => {
+getRequestById: (id) => {
     return VideoRequest.findById({ _id: id });
-  },
+},
 
-  updateRequest: (id, status, resVideo) => {
+updateRequest: (id, status, resVideo) => {
     const updates = {
-      status: status,
-      video_ref: {
+    status: status,
+    video_ref: {
         link: resVideo,
         date: resVideo && new Date(),
-      },
+    },
     };
 
     return VideoRequest.findByIdAndUpdate(id, updates, { new: true });
-  },
+},
 
-  updateVoteForRequest: async (id, vote_type) => {
+updateVoteForRequest: async (id, vote_type, userId) => {
     const oldRequest = await VideoRequest.findById({ _id: id });
     const other_type = vote_type === 'ups' ? 'downs' : 'ups';
-    return VideoRequest.findByIdAndUpdate(
-      { _id: id },
-      {
-        votes: {
-          [vote_type]: ++oldRequest.votes[vote_type],
-          [other_type]: oldRequest.votes[other_type],
-        },
-      }
-    );
-  },
 
-  deleteRequest: (id) => {
+
+    // Convert to set for easier methods
+    oldSelectedVoteArray = new Set(oldRequest.votes[vote_type]); 
+    // If it already includes the user id.. remove him. if not add him
+    oldSelectedVoteArray.has( userId ) ? oldSelectedVoteArray.delete( userId ): oldSelectedVoteArray.add( userId );
+    // Return back to array
+    newSelectedVoteArray = [...oldSelectedVoteArray];
+
+
+    // Delete from other type w 5alas
+    oldOtherVoteArray = new Set(oldRequest.votes[other_type]); 
+    oldOtherVoteArray.delete( userId );
+    // Return back to array
+    newOtherVoteArray = [...oldOtherVoteArray];
+
+
+
+    return VideoRequest.findByIdAndUpdate(
+    { _id: id },
+    {
+        votes: {
+            [vote_type]: newSelectedVoteArray,
+            [other_type]: newOtherVoteArray,
+        },
+    },
+    { new: true }
+    );
+},
+
+deleteRequest: (id) => {
     return VideoRequest.deleteOne({ _id: id });
-  },
+},
 };
